df3d7f3ab5b72773d27176da38349f54dd2cf704
Alex Mañé <alex.manie@outlook.com>
2024-04-10 23:08:43 +0200

    working version with Azure OpenAI

--- gradio-pdf.py
+++ gradio-pdf.py
@@ -1,14 +1,27 @@
 from llama_index.llms.mistralai import MistralAI
+from llama_index.llms.azure_openai import AzureOpenAI
+
 from llama_index.embeddings.mistralai import MistralAIEmbedding
+from llama_index.embeddings.azure_openai import AzureOpenAIEmbedding
+from llama_index.embeddings.openai import (
+    OpenAIEmbedding,
+    OpenAIEmbeddingMode,
+    OpenAIEmbeddingModelType,
+)
+
 from llama_index.core.settings import Settings
 from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
 import gradio as gr
 from gradio_pdf import PDF
 import os
 
-api_key = os.getenv('MISTRAL_API_KEY')
-llm = MistralAI(api_key=api_key, model="mistral-large-latest")
-embed_model = MistralAIEmbedding(model_name='mistral-embed', api_key=api_key)
+from dotenv import load_dotenv
+load_dotenv()
+
+api_key = os.getenv('AZURE_OPENAI_API_KEY')
+azure_endpoint = os.getenv('AZURE_OPENAI_ENDPOINT')
+llm = AzureOpenAI(api_key=api_key, engine="gpt-35-turbo", model="gpt-35-turbo", azure_endpoint=azure_endpoint)
+embed_model = AzureOpenAIEmbedding(azure_deployment='text-embedding-ada-002', model=OpenAIEmbeddingModelType.TEXT_EMBED_ADA_002, api_key=api_key, azure_endpoint=azure_endpoint)
 
 Settings.llm = llm
 Settings.embed_model = embed_model
